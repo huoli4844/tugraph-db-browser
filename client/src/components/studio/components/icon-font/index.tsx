989b533bbf69003ebe50f8a1fcb7c04aafa5f8c2
huoli4844 <161576053@qq.com>
2024-04-19 22:05:10 +0800

    update:处理外部链接

--- client/src/components/studio/components/icon-font/index.tsx
+++ client/src/components/studio/components/icon-font/index.tsx
@@ -1,5 +1,7 @@
 // https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.20&manage_type=myprojects&projectId=3146710&keyword=&project_type=&page=
-import iconFont from './font_3146710_s4on0hqyjeg.js'
+import iconFont from '@/assets/icons/font_3146710_s4on0hqyjeg'
+import nodeIcon from '@/assets/icons/font_2981956_mlh4qdjssyc.js'
+
 import { createFromIconfontCN } from '@ant-design/icons';
 const IconFont = createFromIconfontCN({
   scriptUrl:iconFont
@@ -9,5 +11,6 @@ const IconFont = createFromIconfontCN({
 export default IconFont;
 
 export const NodeIcon = createFromIconfontCN({
-  scriptUrl: 'https://at.alicdn.com/t/a/font_3146710_a0jqa3k8j79.js', // 在 iconfont.cn 上生成
+  scriptUrl:nodeIcon
+  // scriptUrl: 'https://at.alicdn.com/t/a/font_3146710_a0jqa3k8j79.js', // 在 iconfont.cn 上生成
 });
